f482f0a4fd5dfb6af9845c8b23b19124c971828a
Lorran <lorran0202fps@gmail.com>
2023-01-31 20:41:35 -0300

    adiciona novo atributo
    foi adicionado um atributo novo para faciitar o acesso das informações do produto

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 class Produto:
-    def __init__(self,nome,preco):
+    def __init__(self,nome,preco,codigo_de_barras):
         self.nome = nome
         self.preco = preco
+        self.codigo_de_barras = codigo_de_barras
 
 quantidade = int (input('Informe a quantidade: '))
 print(quantidade)
